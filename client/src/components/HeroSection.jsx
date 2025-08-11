6bd1106fea38a85a3cc34831f726f14671fddcf2
vaxi14 <vikasraichandani98@gmail.com>
2025-08-12 01:51:56 +0530

    1:17:09 bit of feautured section done

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -4,9 +4,10 @@ import { ArrowRight, Calendar1Icon, ClockIcon } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
 
-const navigate = useNavigate()
+
 
 const HeroSection = () => {
+    const navigate = useNavigate()
   return (
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
       <img src={assets.marvelLogo} alt="" className='max h-11 lg:h-11 mt-20' />
