01ea370f5b4f780af12fd30d3010c862c0a90e19
vaxi14 <vikasraichandani98@gmail.com>
2025-08-14 23:51:10 +0530

    small

--- client/src/components/TrailersSection.jsx
+++ client/src/components/TrailersSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { dummyTrailers } from '../assets/assets';
 import ReactPlayer from 'react-player';
 import BlurCircle from './BlurCircle';
@@ -25,7 +25,12 @@ const TrailersSection = () => {
       </div>
 
       <div className='group grid grid-cols-2 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
-
+        {dummyTrailers.map((trailer)=>(
+          <div key={trailer.image}>
+            <img src={trailer.image} alt="trailer" className='rounded-lg w-full h-full object-cover brightness-75' />
+            <PLayCircleIcon strokeWidth={1.6} className='absolute top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2' />
+            </div>
+        ))}
       </div>
       
     </div>
