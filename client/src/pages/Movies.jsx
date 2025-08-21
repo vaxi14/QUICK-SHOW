a11e41ff92b981855470c79d9132d3cc93b2efca
vaxi14 <vikasraichandani98@gmail.com>
2025-08-21 13:46:17 +0530

    one

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard'
 
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div>
-
+      <h1>Now Showing</h1>
+      <div>
+        {dummyShowsData.map((movie)=>(
+          <MovieCard movie={movie} key={movie._id}/>
+        ))}
+      </div>
     </div>
   ) : (
     <div>
-      
+
     </div>
   )
 }
