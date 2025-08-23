4a78cad165c2492094b91974bfe861ebc5bb0478
vaxi14 <vikasraichandani98@gmail.com>
2025-08-23 23:18:32 +0530

    css properties

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -4,7 +4,7 @@ import MovieCard from '../components/MovieCard'
 
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
-    <div>
+    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <h1>Now Showing</h1>
       <div>
         {dummyShowsData.map((movie)=>(
