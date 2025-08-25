9e6f9cafe6796c4bb29bca7a7958f6c44478db84
vaxi14 <vikasraichandani98@gmail.com>
2025-08-25 15:29:31 +0530

    sm

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -5,7 +5,7 @@ import MovieCard from '../components/MovieCard'
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
-      <h1>Now Showing</h1>
+      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
       <div>
         {dummyShowsData.map((movie)=>(
           <MovieCard movie={movie} key={movie._id}/>
