4354369ed58ef9da138bdc8d9a5044f1c14b5467
vaxi14 <vikasraichandani98@gmail.com>
2025-08-25 15:30:45 +0530

    cloumn making

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -6,7 +6,7 @@ const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <h1 className='text-lg font-medium my-4'>Now Showing</h1>
-      <div>
+      <div className=''>
         {dummyShowsData.map((movie)=>(
           <MovieCard movie={movie} key={movie._id}/>
         ))}
