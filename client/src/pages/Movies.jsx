deb607da37b8e27ce4b5f5f963956ee2cae53930
vaxi14 <vikasraichandani98@gmail.com>
2025-08-27 01:05:59 +0530

    blurc in movie

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { dummyShowsData } from '../assets/assets'
 import MovieCard from '../components/MovieCard'
+import BlurCircle from '../components/BlurCircle'
 
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+      <BlurCircle/>
       <h1 className='text-lg font-medium my-4'>Now Showing</h1>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.map((movie)=>(
