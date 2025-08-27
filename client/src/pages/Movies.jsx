0fa617171fcdab2891c736c00a37ad7243c140db
vaxi14 <vikasraichandani98@gmail.com>
2025-08-27 20:49:23 +0530

    change1

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -6,7 +6,7 @@ import BlurCircle from '../components/BlurCircle'
 const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
-      <BlurCircle top='150px' left='0pxone m'/>
+      <BlurCircle top='150px' left='0px'/>
       <h1 className='text-lg font-medium my-4'>Now Showing</h1>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.map((movie)=>(
