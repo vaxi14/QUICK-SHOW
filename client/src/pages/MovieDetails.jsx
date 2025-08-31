5f468764ef22d6059f445e7edee25b17a262fffc
vaxi14 <vikasraichandani98@gmail.com>
2025-08-31 21:45:42 +0530

    chn

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const MovieDetails = () => {
+  const {id} = useParams();
+  const [show, setShow] = useState(null);
   return (
     <div>
       
