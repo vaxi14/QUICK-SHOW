3a3e9b068075c0edc7fa08bc601404bc7dfdecfb
vaxi14 <vikasraichandani98@gmail.com>
2025-09-03 23:12:27 +0530

    str2

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { dummyShowsData } from '../assets/assets';
 
 const MovieDetails = () => {
   const {id} = useParams();
   const [show, setShow] = useState(null);
   const getShow = async()=>{
-    
+    const show = dummyShowsData.find(show => show._id ===id)
   }
   return (
     <div>
